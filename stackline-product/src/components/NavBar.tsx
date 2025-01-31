01a15f36b921c5a2a97998c5a32b86eaca557308
Tanmay Patil <patil.tanmay0697@gmail.com>
2025-01-30 20:45:53 -0800

    Create the nav bar

--- stackline-product/src/components/NavBar.tsx
+++ stackline-product/src/components/NavBar.tsx
@@ -1,12 +1,9 @@
-import React from 'react';
-import logo from '../assets/stackline_logo.svg';
+import logo from "../assets/stackline_logo.svg";
 
-const NavBar: React.FC = () => {
-    return (
-        <nav className="flex justify-between items-center p-2.5 bg-[#082957]">
-            <img src={logo} alt="Logo" className="h-10 py-2 mx-4 my-2" />
-        </nav>
-    );
-};
-
-export default NavBar;
+export default function NavBar() {
+  return (
+    <nav className="flex justify-between items-center py-2.5 bg-nav-blue">
+      <img src={logo} alt="Logo" className="h-10 py-2 mx-4 my-2" />
+    </nav>
+  );
+}
